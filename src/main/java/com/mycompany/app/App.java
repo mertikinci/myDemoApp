6bf0c012a1cee866b3aade4456805cb94ad5760d
mertikinci <mertikinci@gmail.com>
2020-02-08 20:41:53 +0300

    started new project

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,43 @@
 package com.mycompany.app;
+import java.util.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static String cryptoMethod(int[] a1,int[] a2, String original,int crypt) {
+		
+		String cryptolized = "";
+		if(a1.length!=a2.length ) {
+			return null;
+		}
+		else if (crypt <=0) {
+			return null;
+		}
+		else if(a1.length!=original.length() || a2.length!=original.length()) {
+			return null;
+			
+		}
+		else {
+			for(int i=0;i<a1.length;i++) {
+				cryptolized += (char)(((original.charAt(i) - 'a' + (a1[i]*a2[i]*crypt))%26)+'a');
+			}
+			
+			return cryptolized;
+			
+		}
+		
+	}
+	// aaaaaa 5 fkpuze
+	// aaaaaa 6 gmsyek
+
+	
+	public static void main(String args[]) {
+		int[] x1 = {1,1,1,1,1,1};
+		int[] x2 = {1,2,3,4,5,6};
+
+		System.out.println(cryptoMethod(x1,x2,"aaaaaa",6));
+		System.out.println(cryptoMethod(x1,x2,"aaaaaaa",5));
+		System.out.println(cryptoMethod(x1,x2,"aaaaaa",-5));
+
+
+	}
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,38 +1,72 @@
 package com.mycompany.app;
-
+import java.util.*;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+public class AppTest extends TestCase{
+    public AppTest(String testname){
+        super(testname);
+        
     }
+    public static Test suite(){
+        return new TestSuite(AppTest.class);
+    }
+    
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	
+	public void testCrypt() {
+		String test = "aaaaaa";
+		int crypt = -3;
+		int[] x1 = {1,1,1,1,1,1};
+		int[] x2 = {1,2,3,4,5,6};
+		
+		assertEquals(null,new App().cryptoMethod(x1, x2, test, crypt));
+		
+		//fail("Not yet implemented");
+	}
+	
+	public void testArrayLengths() {
+		String test = "aaaaaa";
+		int crypt = 3;
+		int[] x1 = {1,1,1,1,1,1};
+		int[] x2 = {1,2,3,4,5};
+		
+		assertEquals(null,new App().cryptoMethod(x1, x2, test, crypt));
+
+	}
+	
+	public void testArrayLengthsWithString() {
+		String test = "aaaa";
+		int crypt = 3;
+		int[] x1 = {1,1,1,1,1,1};
+		int[] x2 = {1,2,3,4,5,6};
+		
+		assertEquals(null,new App().cryptoMethod(x1, x2, test, crypt));
+
+	}
+	
+	public void testResult() {
+		String test = "aaaaaa";
+		int crypt = 5;
+		int[] x1 = {1,1,1,1,1,1};
+		int[] x2 = {1,2,3,4,5,6};
+		
+		assertEquals("fkpuze",new App().cryptoMethod(x1, x2, test, crypt));
+
+	}
+	
+	public void testResult2() {
+		String test = "aaaaaa";
+		int crypt = 6;
+		int[] x1 = {1,1,1,1,1,1};
+		int[] x2 = {1,2,3,4,5,6};
+		
+		assertEquals("gmsyek",new App().cryptoMethod(x1, x2, test, crypt));
+
+	}
+	
+	
+
 }
