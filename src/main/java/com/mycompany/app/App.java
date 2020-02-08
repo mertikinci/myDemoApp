1494a3e099134d53ff4695299c4482e200b65b53
mertikinci <mertikinci@gmail.com>
2020-02-08 21:23:46 +0300

    heroku integration

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,15 @@
 package com.mycompany.app;
-import java.util.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 
 public class App {
 
@@ -28,16 +38,45 @@ public class App {
 	}
 	// aaaaaa 5 fkpuze
 	// aaaaaa 6 gmsyek
+    public static void main(String[] args) {
+        int[] array1 = {1,2,3,4,5,6,7,8,9,10};
+        int[] array2 = {1,2,3,4,5,6,7,8,9,10};
+        String toBeEncrypted = "aaaaaaaaaa";
+        int cryptInt = 92;
 
-	
-	public static void main(String args[]) {
-		int[] x1 = {1,1,1,1,1,1};
-		int[] x2 = {1,2,3,4,5,6};
+        
+        
+        port(getHerokuAssignedPort());
 
-		System.out.println(cryptoMethod(x1,x2,"aaaaaa",6));
-		System.out.println(cryptoMethod(x1,x2,"aaaaaaa",5));
-		System.out.println(cryptoMethod(x1,x2,"aaaaaa",-5));
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
 
 
-	}
+          String result = App.cryptoMethod(array1,array2,toBeEncrypted,cryptInt);
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
